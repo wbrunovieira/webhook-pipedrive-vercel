e97cbbe4cce4e7c2ea4bc83cfc7942ea6cf38a4b
wbrunovieira <wbrunovieira77@gmail.com>
2023-11-11 14:35:48 -0300

    route api/webhook get and post working

--- api/webhook.js
+++ api/webhook.js
@@ -1,16 +1,24 @@
+const express = require('express');
+const app = express();
 const axios = require('axios');
 
-export default function handler(req, res) {
-    if (req.method === 'POST') {
-      const leadData = req.body;
-      console.log('Dados recebidos:', req.body);
-  
-     
-        // ...
-  
-        res.status(200).send('Dados recebidos com sucesso!');
+app.use(express.json());
 
-     
-  
-  }
-}
+app.post('/api/webhook', (req, res) => {
+  // Lógica para manipular a requisição
+  console.log(req.body); // Exibe os dados recebidos
+  res.status(200).send('Requisição recebida');
+});
+
+app.get('/api/webhook', (req, res) => {
+  // Sua lógica para a requisição GET
+  console.log(req.body);
+  res.send('Resposta para GET');
+});
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
+
